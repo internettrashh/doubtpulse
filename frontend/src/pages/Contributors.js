9860307268bd1469b6f7f2879c4986e0163c6cab
pk400 <parthkamat10@gmail.com>
2024-02-10 17:30:40 +0530

    project close

--- frontend/src/pages/Contributors.js
+++ frontend/src/pages/Contributors.js
@@ -1,10 +1,119 @@
 import React from 'react';
-
+import github from "../public/-icon-github.svg";
+import twitter from '../public/-icon-rounded-twitterbird.svg';
+import linkedin from '../public/-icon-rounded-linkedin.svg';
+import figma from '../public/figma.svg';
+import line from '../public/line-5.svg';
 function Contributors() {
   return (
-   <div>
-    
-   </div>
+    <div>
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Bodoni MT:wght@700&display=swap" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Urbanist:wght@400;500;600;700;800&display=swap" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Rammetto One:wght@400&display=swap" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@500;600&display=swap" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inika:wght@700&display=swap" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Alice:wght@400&display=swap" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Alegreya SC:wght@400&display=swap" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Garamond:wght@700&display=swap" />
+    <style dangerouslySetInnerHTML={{__html: "\n      body {\n        margin: 0;\n        line-height: normal;\n      }\n    " }} />
+    <div style={{width: '100%', position: 'relative', backgroundColor: '#1e1e1e', overflow: 'hidden', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start', padding: '27px 51px 134px 65px', boxSizing: 'border-box', gap: '106px', letterSpacing: 'normal'}}>
+      <section style={{display: 'flex', flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'flex-end', padding: '0px 0px 0px 40px', boxSizing: 'border-box', maxWidth: '100%', textAlign: 'left', fontSize: '90px', color: '#fff', fontFamily: 'Alice'}}>
+        <div style={{width: '1572px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start', gap: '26px', maxWidth: '100%'}}>
+          <div style={{width: '1498px', display: 'flex', flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'flex-start', padding: '0px 36px 0px 0px', boxSizing: 'border-box', maxWidth: '100%'}}>
+            <div style={{flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start', maxWidth: '100%'}}>
+              <h1 style={{margin: 0, width: '419px', height: '103px', position: 'relative', fontSize: 'inherit', fontWeight: 400, fontFamily: 'inherit', display: 'inline-block', maxWidth: '100%'}}>
+                Our Team
+              </h1>
+              <img style={{alignSelf: 'stretch', position: 'relative', maxWidth: '100%', overflow: 'hidden', maxHeight: '100%', objectFit: 'contain', marginTop: '-4px'}} loading="eager" alt="" src={line} />
+            </div>
+          </div>
+          <div style={{alignSelf: 'stretch', height: '56px', position: 'relative', fontSize: '23px', fontWeight: 600, fontFamily: 'Urbanist', display: 'inline-block'}}>
+            As budding developers, venturing through the world of Information
+            Technology Engineering in our second year at Goa Engineering
+            College, we craft ingenious solutions for every challenge,
+            transforming the ordinary into the extraordinary!
+          </div>
+        </div>
+      </section>
+      <section style={{alignSelf: 'stretch', display: 'flex', flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'flex-start', padding: '0px 15px 0px 0px', boxSizing: 'border-box', maxWidth: '100%', textAlign: 'left', fontSize: '50px', color: '#fff', fontFamily: '"Alegreya SC"'}}>
+        <div style={{flex: 1, overflowX: 'auto', display: 'flex', flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'space-between', padding: '0px 1px 0px 0px', boxSizing: 'border-box', gap: '20px', maxWidth: '100%'}}>
+          <div style={{width: '464px', borderRadius: '30px 0px 30px 0px', backgroundColor: '#1e1e1e', boxShadow: '0px 5px 10px #665b86', border: '1px solid #665b86', boxSizing: 'border-box', flexShrink: 0, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start', padding: '58px 53px 53px', gap: '21px'}}>
+            <div style={{width: '464px', height: '664px', position: 'relative', borderRadius: '30px 0px 30px 0px', backgroundColor: '#1e1e1e', boxShadow: '0px 5px 10px #665b86', border: '1px solid #665b86', boxSizing: 'border-box', display: 'none', maxWidth: '100%'}} />
+            <img style={{width: '300px', height: '300px', position: 'relative', borderRadius: '50%', objectFit: 'cover', zIndex: 1}} loading="eager" alt="" src={require ("../public/contributor2.jpeg")} />
+            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start', padding: '0px 10px 13px'}}>
+              <div style={{height: '68px', position: 'relative', display: 'inline-block', zIndex: 1}}>
+                Nischal Naik
+              </div>
+              <b style={{position: 'relative', fontSize: '40px', display: 'inline-block', fontFamily: 'Garamond', zIndex: 1, marginTop: '-11px'}}>Back-end developer</b>
+            </div>
+            <div style={{display: 'flex', flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'flex-start', padding: '0px 19px', gap: '12px'}}>
+              
+            <a href='https://github.com/internettrashh'> <img style={{cursor:'pointer',alignSelf: 'stretch', width: '102.4px', position: 'relative', maxHeight: '100%', minHeight: '96px', zIndex: 1}} alt="" src={github} /></a>
+
+              <div style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start', padding: '3px 11px 0px 0px'}}>
+               <a href='https://twitter.com/naik_nischal'>
+               <img style={{cursor:'pointer',width: '90.3px', height: '90.3px', position: 'relative', zIndex: 1}} loading="eager" alt="" src={twitter }/>
+                </a> 
+              </div>
+             <a href='https://www.linkedin.com/in/nischal-naik-a188b0288/' ><img style={{cursor:'pointer',height: '90.3px', width: '90.3px', position: 'relative', zIndex: 1}} loading="eager" alt="" src={linkedin} /></a>
+            </div>
+          </div>
+          <div style={{width: '464px', borderRadius: '30px 0px 30px 0px', backgroundColor: '#1e1e1e', boxShadow: '0px 5px 10px #665b86', border: '1px solid #665b86', boxSizing: 'border-box', flexShrink: 0, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start', padding: '58px 60px 48px 59px', gap: '21px'}}>
+            <div style={{width: '464px', height: '664px', position: 'relative', borderRadius: '30px 0px 30px 0px', backgroundColor: '#1e1e1e', boxShadow: '0px 5px 10px #665b86', border: '1px solid #665b86', boxSizing: 'border-box', display: 'none', maxWidth: '100%'}} />
+            <div style={{alignSelf: 'stretch', display: 'flex', flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'flex-start', padding: '0px 12px 0px 31px'}}>
+              <img style={{height: '300px', flex: 1, position: 'relative', borderRadius: '50%', maxWidth: '100%', overflow: 'hidden', objectFit: 'cover', zIndex: 1}} loading="eager" alt="" src={require ("../public/contributor.jpeg")} />
+            </div>
+            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start', padding: '0px 0px 18px'}}>
+              <div style={{height: '68px', position: 'relative', display: 'inline-block', zIndex: 1}}>
+                Parth Kamat
+              </div>
+              <b style={{position: 'relative', fontSize: '40px', display: 'inline-block', fontFamily: 'Garamond', zIndex: 1, marginTop: '-11px'}}>Front-end developer</b>
+            </div>
+            <div   style={{display: 'flex', flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'flex-start', padding: '0px 13px 0px 12px', gap: '12px'}}>
+            
+            <a href='https://github.com/cazrot335'> <img style={{cursor:'pointer',alignSelf: 'stretch', width: '102.4px', position: 'relative', maxHeight: '100%', minHeight: '96px', zIndex: 1}} alt="" src={github} /></a>
+            
+             
+              <div style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start', padding: '3px 11px 0px 0px'}}>
+               
+               <a href='https://twitter.com/bay_Watcher69'>
+               <img style={{cursor:'pointer',width: '90.3px', height: '90.3px', position: 'relative', zIndex: 1}} alt="" src={twitter} />
+               </a>
+               
+              </div>
+              <a href='https://www.linkedin.com/feed/'>
+              <img style={{cursor:'pointer',height: '90.3px', width: '90.3px', position: 'relative', zIndex: 1}} alt="" src={linkedin} />
+              </a>
+              
+            </div>
+          </div>
+          <div style={{width: '464px', borderRadius: '30px 0px 30px 0px', backgroundColor: '#1e1e1e', boxShadow: '0px 5px 10px #665b86', border: '1px solid #665b86', boxSizing: 'border-box', flexShrink: 0, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start', padding: '58px 50px 50px', gap: '21px'}}>
+            <div style={{width: '464px', height: '664px', position: 'relative', borderRadius: '30px 0px 30px 0px', backgroundColor: '#1e1e1e', boxShadow: '0px 5px 10px #665b86', border: '1px solid #665b86', boxSizing: 'border-box', display: 'none', maxWidth: '100%'}} />
+            <img style={{width: '300px', height: '300px', position: 'relative', borderRadius: '50%', objectFit: 'cover', zIndex: 1}} loading="eager" alt="" src={require('../public/contributor3.jpeg')} />
+            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-start', padding: '0px 0px 13px'}}>
+              <div style={{height: '68px', position: 'relative', display: 'inline-block', zIndex: 1}}>
+                Meher Aga
+              </div>
+              <b style={{position: 'relative', fontSize: '40px', display: 'inline-block', fontFamily: 'Garamond', zIndex: 1, marginTop: '-11px'}}>UI UX designer</b>
+            </div>
+            <div style={{alignSelf: 'stretch', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', padding: '0px 25px', gap: '21px'}}>
+              <div style={{flex: 1, display: 'flex', flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'flex-start', gap: '9px', minWidth: '131px'}}>
+                <div style={{flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-start', padding: '0px 0px 3px', boxSizing: 'border-box', minWidth: '66px'}}>
+                 <a href='https://github.com/meheraga'> <img style={{cursor:'pointer',alignSelf: 'stretch', height: '96px', position: 'relative', maxWidth: '100%', overflow: 'hidden', flexShrink: 0, zIndex: 1}} alt="" src={github} /></a>
+                </div>
+                <a href='https://www.figma.com/file/tPZx0UwonslwOkDPRpuXYG/DoubtPulse?type=design&node-id=0%3A1&mode=design&t=PlZzCbEI0NJUDUqZ-1'>
+                <img style={{cursor:'pointer',height: '90.3px', width: '90.3px', position: 'relative', zIndex: 1}} loading="eager" alt="" src={figma} /></a>
+              </div>
+              <a href='https://www.linkedin.com/in/meher-aga-6807b5299/'>
+              <img style={{cursor:'pointer',height: '90.3px', width: '90.3px', position: 'relative', zIndex: 1}} alt="" src={linkedin} /></a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
   );
 }
 
